179b80effcc17c3cfb163b22ed033db838b537f8
point-o <ryanpointer@live.com>
2025-07-30 20:01:16 -0600

    launcher for dsh

---
+++ DSH-REPL/src/dsh/Main.java
@@ -0,0 +1,13 @@
+package dsh;
+
+/**
+ * A main to run dsh
+ * @author Ryan Pointer
+ * @version 7/30/25
+ */
+public class Main {
+    public static void main(String[] args) {
+        DSHRepl repl = new DSHRepl();
+        repl.start();
+    }
+}
